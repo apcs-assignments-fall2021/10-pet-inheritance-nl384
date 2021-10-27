93cfe30881e9d1c1a82200045efd04155da0a500
Nathan Lin <nlin@athenian.org>
2021-10-27 12:12:27 -0700

    first draft

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,35 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog() {
+        super();
+        this.breed = "Mixed";
+    }
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
 
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Bow wow");
+    }
 
     // toString method
-
+    public String toString() {
+        return super.toString() + ", Breed: " + this.breed;
+    }
 
     // Getter
-
+    public String getBreed() {
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
 }
